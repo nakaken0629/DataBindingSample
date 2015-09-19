15b556309c66e4a772f3471fb5d8ad266e6f0b37
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-09-19 09:53:02 +0900

    Initial commit

---
+++ app/src/main/java/red/itvirtuoso/databindingsample/User.java
@@ -0,0 +1,13 @@
+package red.itvirtuoso.databindingsample;
+
+/**
+ * Created by kenji on 15/09/19.
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
